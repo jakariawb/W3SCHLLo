6b3508e756bf4f58b53ff4b74ac32141f507413a
JAKARIA JOMMADER <166798315+jakariawb@users.noreply.github.com>
2025-05-11 22:51:43 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+// just number
+var n = 1221;
+console.log(n)
+
+// folating Number
+var f = 22.33;
+console.log(f)
+
+// MAX NUmber 1.7976931348623157e+308
+console.log(Number.MAX_VALUE)
+
+// number min safe integer
+console.log(Number.MIN_SAFE_INTEGER)
+
+// out put infinity
+console.log(1/0);
+
+// out put NAN
+console.log("an"*12)
+
+// -----string------
+// string literal
+var name1 = "jakaria";
+var profession1 = "developer"
+console.log(name1,profession1)
+
+var name = ("jakaria")
+var woliteName = ("levis's")
+
+// number convert for string
+// var favouriteNumber = string(10);
+// var angryNumber = string(222.0);
+// console.log(favouriteNumber, angryNumber)
+
+// bulien datatype
+var b1 = true;
+var b2 = false;
+
+console.log(b1,b2);
