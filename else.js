d283646831fac50afe09e2643d946a273b3db942
JAKARIA JOMMADER <166798315+jakariawb@users.noreply.github.com>
2025-05-13 22:20:13 +0600

    first commit

---
+++ else.js
@@ -0,0 +1,45 @@
+var age = 50;
+if (age % 2 === 0){
+    console.log(age + "Even Number")
+} 
+else{
+    console.log(age + "add Number")
+}
+
+var number = 30;
+var b = 30;
+
+if (number > b){
+    console.log(number + 'first number')
+}
+else if(number < b){
+    console.log(b + 'seckind number')
+}
+else if(number === b){
+    console.log(number + " = " + b + " saime Number")
+}
+else{
+    console.log("tha boter same")
+}
+
+var dinner = "11:pm";
+var leince = "2:pm";
+if(dinner){
+     console.log("ami khabo")
+}else{
+   console.lgo("ami khabo na")
+}
+
+var n = 5;
+var a = 5;
+if(n > a){
+   console.log("N Number A number ")
+}
+else if (a < n){
+     console.log("A number is bige")
+}
+else if (n == a){
+    console.log("Number N  and Number A")
+}else{
+  console.log("the boter seme")
+}
---
+++ if.js
@@ -0,0 +1,28 @@
+var a = 10;
+var b = 20;
+
+if (a < b) {
+   console.log("A is greater than A")
+}
+if(b > a){
+   console.log("B is greater than A")
+}
+
+var n = 5;
+if (n % 2 === 0){
+   console.log(n +'even Number')
+}
+if (n % 2 === 1){
+   console.log(n + 'Add Number')
+}
+
+
+var yourAge = 25;
+
+yourAge = 30
+if (yourAge % 2 === 0){
+   console.log(yourAge + "My Age is Even Number");
+}
+if (yourAge % 2 === 1){
+   console.log(yourAge + "your Age Odd Number");
+}
